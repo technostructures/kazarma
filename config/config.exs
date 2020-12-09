ace3514ce397b0416cc74d9d2ae43a35a7a3f6ec
Pierre de Lacroix <pierre@pdelacroix.com>
2020-12-09 13:45:40 +0100

    enable color in logs

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,8 @@ config :kazarma, KazarmaWeb.Endpoint,
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+  metadata: [:request_id],
+  colors: [enabled: true]
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
