44d2d4106bf9d45c6c66396ed8e450badb0e8f2d
Pierre de Lacroix <pierre@pdelacroix.com>
2021-01-24 17:10:47 +0100

    disable pleroma's remoteIp plug

--- infra/dev/pleroma/config.exs
+++ infra/dev/pleroma/config.exs
@@ -2,14 +2,15 @@ import Config
 
 config :pleroma, Pleroma.Web.Endpoint,
   url: [host: System.get_env("DOMAIN", "localhost"), scheme: "http", port: 80],
-    http: [ip: {0, 0, 0, 0}, port: 4000]
+  http: [ip: {0, 0, 0, 0}, port: 4000]
 
 config :pleroma, :instance,
   federating: true,
   registrations_open: true
 
-config :pleroma, Pleroma.Captcha,
-  enabled: false
+config :pleroma, Pleroma.Captcha, enabled: false
+
+config :pleroma, Pleroma.Web.Plugs.RemoteIp, enabled: false
 
 config :logger, :console, level: :warn
 config :logger, :ex_syslogger, level: :warn
