2a2e9b1f3bac817eafb16afaa5f3098c5d0d232e
Pierre de Lacroix <pierre@pdelacroix.com>
2022-10-21 22:20:56 +0200

    fix dev domain issue

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ import Mix.Config
 config :kazarma, Kazarma.Repo,
   username: System.get_env("POSTGRES_USER") || "postgres",
   password: System.get_env("POSTGRES_PASSWORD") || "postgres",
-  database: "kazarma_dev",
+  database: System.get_env("POSTGRES_DB") || "kazarma_dev",
   hostname: System.get_env("POSTGRES_HOST") || "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
