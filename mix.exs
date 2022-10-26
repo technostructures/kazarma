a736aae3b55ce19f6e26dbb31bc738c349ceb574
Pierre de Lacroix <pierre@pdelacroix.com>
2022-10-26 19:10:20 +0200

    add html_sanitize_ex dependency

--- mix.exs
+++ mix.exs
@@ -70,7 +70,8 @@ defmodule Kazarma.MixProject do
       {:logger_file_backend, "~> 0.0.13"},
       {:sentry, "~> 8.0"},
       {:prom_ex, "~> 1.7"},
-      {:floki, ">= 0.30.0"}
+      {:floki, ">= 0.30.0"},
+      {:html_sanitize_ex, "~> 1.4"}
     ]
   end
 
