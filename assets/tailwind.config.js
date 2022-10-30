bcfb1621d24065b630c686a5c01a6405c96e664b
Pierre de Lacroix <pierre@pdelacroix.com>
2022-10-30 04:52:59 +0100

    improve object web view

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
+    require('@tailwindcss/typography'),
     require('daisyui')
   ],
 }
--- lib/kazarma_web/controllers/object_controller.ex
+++ lib/kazarma_web/controllers/object_controller.ex
@@ -6,19 +6,12 @@ defmodule KazarmaWeb.ObjectController do
 
   def show(conn, %{"uuid" => uuid}) do
     case ActivityPub.Object.get_by_id(uuid) do
-      nil ->
-        conn
-        |> fetch_session()
-        |> fetch_flash()
-        |> protect_from_forgery()
-        |> put_layout({KazarmaWeb.LayoutView, "app.html"})
-        |> put_flash(:error, gettext("Object not found"))
-        |> redirect(to: Routes.index_path(conn, :index))
-
-      object ->
+      %{data: %{"type" => "Note"}} = object ->
         {:ok, actor} =
           object.data["actor"]
           |> ActivityPub.Actor.get_or_fetch_by_ap_id()
+
+        previous_objects = traverse_replies_to(object) |> Enum.reverse()
 
         conn
         |> fetch_session()
@@ -26,8 +19,33 @@ defmodule KazarmaWeb.ObjectController do
         |> protect_from_forgery()
         |> put_layout({KazarmaWeb.LayoutView, "app.html"})
         |> put_view(KazarmaWeb.ObjectView)
-        # TODO: title
-        |> render("show.html", object: object, actor: actor, title: "title")
+        |> render("show.html",
+          object: object,
+          previous_objects: previous_objects,
+          actor: actor,
+          title: KazarmaWeb.ObjectView.text_content(object) |> String.slice(0, 60)
+        )
+
+      _ ->
+        conn
+        |> fetch_session()
+        |> fetch_flash()
+        |> protect_from_forgery()
+        |> put_layout({KazarmaWeb.LayoutView, "app.html"})
+        |> put_flash(:error, gettext("Object not found"))
+        |> redirect(to: Routes.index_path(conn, :index))
     end
   end
+
+  defp traverse_replies_to(%{data: %{"inReplyTo" => reply_to_id}}) do
+    case ActivityPub.Object.get_cached_by_ap_id(reply_to_id) do
+      %{data: %{"type" => "Note"}} = reply_to ->
+        [reply_to | traverse_replies_to(reply_to)]
+
+      _ ->
+        []
+    end
+  end
+
+  defp traverse_replies_to(_), do: []
 end
