2eed34c6be9d240c3d7d3aaeb5de9d216d08b000
Pierre de Lacroix <pierre@pdelacroix.com>
2022-11-28 20:18:40 +0100

    remove dead code

--- lib/kazarma/matrix/transaction.ex
+++ lib/kazarma/matrix/transaction.ex
@@ -102,27 +102,6 @@ defmodule Kazarma.Matrix.Transaction do
     :ok
   end
 
-  def new_event(%Event{
-        type: "m.room.member",
-        content: %{"membership" => "leave"},
-        room_id: room_id,
-        state_key: user_id
-      }) do
-    case Kazarma.Address.matrix_id_to_actor(user_id) do
-      {:ok, %ActivityPub.Actor{local: true} = _follower} ->
-        case Bridge.get_room_by_local_id(room_id) do
-          %Room{data: %{"type" => "actor_outbox"}, remote_id: followed_ap_id} ->
-            {:ok, _followed} = ActivityPub.Actor.get_or_fetch_by_ap_id(followed_ap_id)
-
-          _ ->
-            nil
-        end
-
-      _ ->
-        :ok
-    end
-  end
-
   def new_event(
         %Event{
           type: "m.room.member",
