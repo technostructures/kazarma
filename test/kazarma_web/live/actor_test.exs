7bda4304654f049022fccdc2aaa0243c08e371a6
Pierre de Lacroix <pierre@pdelacroix.com>
2023-10-17 17:46:48 +0200

    improve actor liveview test

--- test/kazarma_web/live/actor_test.exs
+++ test/kazarma_web/live/actor_test.exs
@@ -11,7 +11,7 @@ defmodule KazarmaWeb.ActorTest do
 
   alias Kazarma.Bridge
 
-  describe "displaying a Matrix user page" do
+  describe "when browsing a Matrix user page" do
     setup :set_mox_from_context
     setup :verify_on_exit!
 
@@ -104,7 +104,7 @@ defmodule KazarmaWeb.ActorTest do
     end
   end
 
-  describe "displaying an ActivityPub user page" do
+  describe "when browsing an ActivityPub user page" do
     setup :set_mox_from_context
     setup :verify_on_exit!
 
@@ -131,7 +131,12 @@ defmodule KazarmaWeb.ActorTest do
       |> ActivityPub.Actor.set_cache()
 
       for x <- 1..25 do
-        ActivityPub.Object.insert(%{
+        {:ok, inserted_at} = NaiveDateTime.new(2023, 10, 17, 12, 0, x)
+
+        x = if(x < 10, do: "0#{x}", else: "#{x}")
+
+        Kazarma.Repo.insert!(%ActivityPub.Object{
+          inserted_at: inserted_at,
           data: %{
             "id" => "http://kazarma/-/alice/note/note#{x}",
             "actor" => "http://pleroma/pub/actors/alice",
@@ -145,7 +150,7 @@ defmodule KazarmaWeb.ActorTest do
       :ok
     end
 
-    test "shows user info", %{conn: conn} do
+    test "it shows user info", %{conn: conn} do
       {:ok, _view, html} = live(conn, "/pleroma/alice")
 
       assert html =~ "Alice"
@@ -154,25 +159,7 @@ defmodule KazarmaWeb.ActorTest do
       assert html =~ "This user is not bridged."
     end
 
-    test "shows user public activities if the user is bridged", %{conn: conn} do
-      {:ok, _room} =
-        Bridge.create_room(%{
-          data: %{"matrix_id" => "@alice:kazarma", "type" => "ap_user"},
-          local_id: "!foo:kazarma",
-          remote_id: "http://pleroma/pub/actors/alice"
-        })
-
-      {:ok, _view, html} = live(conn, "/pleroma/alice")
-
-      html
-      |> assert_html_include("div[id=\"http://kazarma/-/alice/note/note1\"")
-      |> assert_html_include("div[id=\"http://kazarma/-/alice/note/note2\"")
-      |> assert_html_include("div[id=\"http://kazarma/-/alice/note/note11\"", 0)
-
-      # Todo: add test to ensure just tens activities are displaied
-    end
-
-    test "handles 'load_more' events", %{conn: conn} do
+    test "it shows paginated public activities if the user is bridged", %{conn: conn} do
       {:ok, _room} =
         Bridge.create_room(%{
           data: %{"matrix_id" => "@alice:kazarma", "type" => "ap_user"},
@@ -182,20 +169,124 @@ defmodule KazarmaWeb.ActorTest do
 
       {:ok, view, html} = live(conn, "/pleroma/alice")
 
-      view
-      |> render_hook(:load_more)
+      assert html =~ "Note 25"
+      assert html =~ "Note 24"
+      assert html =~ "Note 23"
+      assert html =~ "Note 22"
+      assert html =~ "Note 21"
+      assert html =~ "Note 20"
+      assert html =~ "Note 19"
+      assert html =~ "Note 18"
+      assert html =~ "Note 17"
+      assert html =~ "Note 16"
 
-      view
-      |> render_hook(:load_more)
+      refute html =~ "Note 15"
+      refute html =~ "Note 14"
+      refute html =~ "Note 13"
+      refute html =~ "Note 12"
+      refute html =~ "Note 11"
+      refute html =~ "Note 10"
+      refute html =~ "Note 09"
+      refute html =~ "Note 08"
+      refute html =~ "Note 07"
+      refute html =~ "Note 06"
+      refute html =~ "Note 05"
+      refute html =~ "Note 04"
+      refute html =~ "Note 03"
+      refute html =~ "Note 02"
+      refute html =~ "Note 01"
 
-      html = render(view)
+      html = view |> element("button", "...") |> render_click()
 
-      html
-      |> assert_html_include("div[id=\"http://kazarma/-/alice/note/note4\"")
-      |> assert_html_include("div[id=\"http://kazarma/-/alice/note/note14\"")
-      |> assert_html_include("div[id=\"http://kazarma/-/alice/note/note24\"")
-      |> assert_html_include("div[id=\"http://kazarma/-/alice/note/note35\"", 0)
-      |> refute_html_include("button", %{}, "load more")
+      assert html =~ "Note 25"
+      assert html =~ "Note 24"
+      assert html =~ "Note 23"
+      assert html =~ "Note 22"
+      assert html =~ "Note 21"
+      assert html =~ "Note 20"
+      assert html =~ "Note 19"
+      assert html =~ "Note 18"
+      assert html =~ "Note 17"
+
+      assert html =~ "Note 16"
+      assert html =~ "Note 15"
+      assert html =~ "Note 14"
+      assert html =~ "Note 13"
+      assert html =~ "Note 12"
+      assert html =~ "Note 11"
+      assert html =~ "Note 10"
+      assert html =~ "Note 09"
+      assert html =~ "Note 08"
+      assert html =~ "Note 07"
+      assert html =~ "Note 06"
+
+      refute html =~ "Note 05"
+      refute html =~ "Note 04"
+      refute html =~ "Note 03"
+      refute html =~ "Note 02"
+      refute html =~ "Note 01"
+
+      html = view |> element("button", "...") |> render_click()
+
+      assert html =~ "Note 25"
+      assert html =~ "Note 24"
+      assert html =~ "Note 23"
+      assert html =~ "Note 22"
+      assert html =~ "Note 21"
+      assert html =~ "Note 20"
+      assert html =~ "Note 19"
+      assert html =~ "Note 18"
+      assert html =~ "Note 17"
+
+      assert html =~ "Note 16"
+      assert html =~ "Note 15"
+      assert html =~ "Note 14"
+      assert html =~ "Note 13"
+      assert html =~ "Note 12"
+      assert html =~ "Note 11"
+      assert html =~ "Note 10"
+      assert html =~ "Note 09"
+      assert html =~ "Note 08"
+      assert html =~ "Note 07"
+      assert html =~ "Note 06"
+
+      assert html =~ "Note 05"
+      assert html =~ "Note 04"
+      assert html =~ "Note 03"
+      assert html =~ "Note 02"
+      assert html =~ "Note 01"
+
+      refute html =~ "..."
+    end
+  end
+
+  describe "when searching from a user page" do
+    setup :set_mox_from_context
+    setup :verify_on_exit!
+
+    setup do
+      # {:ok, keys} = ActivityPub.Keys.generate_rsa_pem()
+
+      {:ok, actor} =
+        ActivityPub.Object.insert(%{
+          "data" => %{
+            "type" => "Person",
+            "name" => "Alice",
+            "preferredUsername" => "alice",
+            "url" => "http://pleroma/pub/actors/alice",
+            "id" => "http://pleroma/pub/actors/alice",
+            "username" => "alice@pleroma"
+          },
+          "local" => false,
+          "public" => true,
+          "actor" => "http://pleroma/pub/actors/alice"
+        })
+
+      actor
+      |> ActivityPub.Actor.format_remote_actor()
+      |> ActivityPub.Actor.set_cache()
+
+      :ok
     end
 
     test "handles 'search' events with a redirect when the user exist", %{conn: conn} do
@@ -214,7 +305,7 @@ defmodule KazarmaWeb.ActorTest do
       assert_receive {_, {:redirect, _, %{kind: :push, to: "/pleroma/alice"}}}
     end
 
-    test "handles 'search'  with an error when the user is not found", %{conn: conn} do
+    test "it handles 'search' with an error when the user is not found", %{conn: conn} do
       {:ok, _room} =
         Bridge.create_room(%{
           data: %{"matrix_id" => "@alice:kazarma", "type" => "ap_user"},
@@ -231,7 +322,7 @@ defmodule KazarmaWeb.ActorTest do
                live(conn, "/pleroma/not_found")
     end
 
-    test "shows an error and redirects if the user is not found", %{conn: conn} do
+    test "it shows an error and redirects if the user is not found", %{conn: conn} do
       assert {:error, {:live_redirect, %{to: "/", flash: %{"error" => "User not found"}}}} =
                live(conn, "/pleroma/not_found")
     end
